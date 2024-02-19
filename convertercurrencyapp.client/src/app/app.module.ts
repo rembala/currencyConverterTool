23fb3afaa0e47e87e5e93627c5591246559f77dc
Artur <maslovskij.artur@gmail.com>
2024-02-19 15:18:58 +0200

    DDD projects

--- convertercurrencyapp.client/src/app/app.module.ts
+++ convertercurrencyapp.client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyConveterComponent } from './components/currency-conveter/currency-conveter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyConveterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
